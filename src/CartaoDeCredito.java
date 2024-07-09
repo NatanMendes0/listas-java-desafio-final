c88c6176b02506556f8fba7533d79125b330aeb7
Natan Mendes <natancmendes@gmail.com>
2024-07-09 16:04:04 -0300

    add classes

--- src/CartaoDeCredito.java
+++ src/CartaoDeCredito.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class CartaoDeCredito {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite, saldo;
+    private List<Compra> compras;
+
+    //sempre inicializar uma instancia com um limite
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    /*
+        A função lancaCompra (abaixo) recebe uma nova compra e verifica se o saldo da conta é maior ou igual o valor da compra,
+        retornando true. Se for menor, não deixa o usuário criar a compra
+     */
+
+    public boolean lancaCompra(Compra compra){
+        if(this.saldo >= compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
 }
